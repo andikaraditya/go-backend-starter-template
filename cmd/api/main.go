1bab5f9f7786338de6a50586280288247684e0b8
andikaraditya <andikaraditya1@gmail.com>
2024-09-21 14:21:11 +0700

    init fiber and pgx

---
+++ cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(logger.New(logger.Config{
+		Format:        "${time} | ${status} | ${latency} | ${ip} | ${method} | ${path} | ${queryParams} | ${error}\n",
+		TimeFormat:    "15:04:05",
+		TimeZone:      "Local",
+		TimeInterval:  500 * time.Millisecond,
+		Output:        os.Stdout,
+		DisableColors: false,
+	}))
+
+	app.Use(recover.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Listen(":3000")
+}
